b911e184a5cd30e743c896a88b8b853b0dc34d75
Siddharth <sidaws35@gmail.com>
2025-07-07 12:28:07 -0400

    Update the Port

--- development/main.tf
+++ development/main.tf
@@ -23,7 +23,7 @@ module "dev_vpc_1" {
 module "dev_sg_1" {
   source             = "../modules/sg"
   vpc_name           = module.dev_vpc_1.vpc_name
-  ingress_port_value = ["80", "443", "22"]
+  ingress_port_value = ["80", "443", "22","8080"]
   vpc_id             = module.dev_vpc_1.vpc_id
   environment        = module.dev_vpc_1.environment
 }
